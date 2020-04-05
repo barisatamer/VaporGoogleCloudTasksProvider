6db9505117c0c56cfc6e2909ae9a468e97f080bf
Baris Atamer <brsatamer@gmail.com>
2020-04-05 03:44:46 +0200

    test(tasksProviderTest): remove VaporGoogleCloudTasksProvider().text

--- Tests/VaporGoogleCloudTasksProviderTests/VaporGoogleCloudTasksProviderTests.swift
+++ Tests/VaporGoogleCloudTasksProviderTests/VaporGoogleCloudTasksProviderTests.swift
@@ -6,7 +6,7 @@ final class VaporGoogleCloudTasksProviderTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(VaporGoogleCloudTasksProvider().text, "Hello, World!")
+        
     }
 
     static var allTests = [
